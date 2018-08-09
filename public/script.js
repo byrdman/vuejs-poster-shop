c66ec61deb275f87135a4a6533501bfb3a3f5a01
Sam Byrd <sbyrd@eeft>
2018-08-08 21:22:31 -0500

    finished product

--- public/script.js
+++ public/script.js
@@ -1,66 +1,100 @@
-new Vue ({
-   el: '#app',
-   data: {
-      total: 0,
-      items: [
-         { id: 1, title: 'Item 1', price: 1.11 },
-         { id: 2, title: 'Item 2', price: 2.22 },
-         { id: 3, title: 'Item 3', price: 3.33}
-       ],
-       cart: [],
-       search: ''
-   },
-   methods: {
-      onSubmit: function() {
-          console.log("onSubmit");
-          this.$http
-             .get('/search/'.concat('90s'))
-             .then(function(res) {
-                 console.log(res);
-             });
-      } ,
-      addItem: function(index) {
-         console.log("addItem: "  + index);
-         var item = this.items[index];
-         this.total += item.price;
-         var found = false;
-         for (var i = 0; i < this.cart.length; i++) {
-            if (this.cart[i].id === item.id) {
-               found = true;
-               this.cart[i].qty++;
-               break;
-            }
-         }
+var PRICE = 9.99;
+var LOADNUM = 10;
 
-         if (!found) {
-             this.cart.push({
-                 id: item.id,
-                 title: item.title,
-                 qty: 1,
-                 price: item.price
-             });
-         }
-      },
-       inc: function(item) {
-          item.qty++;
-          this.total += item.price;
-       },
-       dec: function(item) {
-          item.qty--;
-          this.total -= item.price;
-          if (item.qty === 0) {
-              for (var i = 0; i < this.cart.length; i++) {
-                  if (this.cart[i].id === item.id) {
-                      this.cart.splice(i, 1);
-                      break;
-                  }
-              }
-          }
-       }
-   },
-    filters: {
-       currency: function(price) {
-           return '$'.concat(price.toFixed(2));
-       }
+new Vue({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [],
+    cart: [],
+    results: [],
+    newSearch: 'anime',
+    lastSearch: '',
+    loading: false,
+    price: PRICE
+
+  },
+  computed: {
+    noMoreItems: function () {
+      return this.items.length === this.results.length && this.results.length > 0;
     }
+  },
+  methods: {
+    appendItems: function () {
+      if (this.items.length < this.results.length) {
+        var append = this.results.slice(this.items.length, this.items.length + LOADNUM);
+        this.items = this.items.concat(append);
+      }
+    },
+    onSubmit: function () {
+      if (this.newSearch.length) {
+        this.items = [];
+        this.loading = true;
+        this.$http
+          .get('/search/'.concat(this.newSearch))
+          .then(function (res) {
+            this.lastSearch = this.newSearch;
+            this.results = res.data;
+            this.appendItems();
+            this.loading = false;
+            console.log(res);
+          })
+        ;
+      }
+    },
+    addItem: function (index) {
+      console.log("addItem: " + index);
+      var item = this.items[index];
+      item.price = PRICE;
+      this.total += PRICE;
+      var found = false;
+      for (var i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].id === item.id) {
+          found = true;
+          this.cart[i].qty++;
+          break;
+        }
+      }
+
+      if (!found) {
+        this.cart.push({
+          id: item.id,
+          title: item.title,
+          qty: 1,
+          price: item.price
+        });
+      }
+    },
+    inc: function (item) {
+      item.qty++;
+      this.total += item.price;
+    },
+    dec: function (item) {
+      item.qty--;
+      this.total -= item.price;
+      if (item.qty === 0) {
+        for (var i = 0; i < this.cart.length; i++) {
+          if (this.cart[i].id === item.id) {
+            this.cart.splice(i, 1);
+            break;
+          }
+        }
+      }
+    }
+  },
+  filters: {
+    currency: function (price) {
+      return '$'.concat(price.toFixed(2));
+    }
+  },
+  mounted: function () {
+    this.onSubmit();
+    var vueInstance = this;
+    var elem = document.getElementById("product-list-bottom");
+    var watcher = scrollMonitor.create(elem);
+    watcher.enterViewport(function () {
+      vueInstance.appendItems();
+    });
+  }
 });
+
